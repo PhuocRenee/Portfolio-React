562282a0276ca9621c9f3a4499a8e482995ca94a
puokl <phuoc@sapo.pt>
2022-10-26 13:43:20 +0200

    contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Contact from "./components/Contact/Contact";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <div>Hello world</div>
+      <Contact />
+    </>
+  );
 }
 
 export default App;
