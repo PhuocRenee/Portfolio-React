46be64ea3bc59381e65e37a7727648e40af0edf1
puokl <phuoc@sapo.pt>
2022-10-31 14:53:46 +0100

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Contact from "./components/Contact/Contact";
+import Topbar from "./components/Topbar/Topbar";
+import Menu from "./components/Menu/Menu";
+import Intro from "./components/Intro/Intro";
+import Projects from "./components/Projects/Projects";
+import "./app.scss";
 
 function App() {
   return (
     <>
       <div>Hello world</div>
-      <Contact />
+      <div className="app">
+        <Topbar />
+        <Menu />
+      </div>
+      <div className="sections">
+        <Intro />
+        <Projects />
+        <Contact />
+      </div>
     </>
   );
 }
