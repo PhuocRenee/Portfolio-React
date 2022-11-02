8316796a6e5b9d5ac34fb783724b333ee067a485
RvVelthuijsen <r.v.velthuijsen@gmail.com>
2022-11-02 19:46:30 +0100

    layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import Contact from "./components/Contact/Contact";
 import Topbar from "./components/Topbar/Topbar";
 import Menu from "./components/Menu/Menu";
-import Intro from "./components/Intro/Intro";
+import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import "./app.scss";
 
 function App() {
   return (
     <>
-      <div>Hello world</div>
       <div className="app">
         <Topbar />
-        <Menu />
       </div>
       <div className="sections">
-        <Intro />
         <Projects />
+        <About />
         <Contact />
       </div>
     </>
